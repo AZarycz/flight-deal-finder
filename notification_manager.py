937d8f0ae1a290333139d210bb3cb3c6a97942f9
Anna Zaryczanska <annazar00@gmail.com>
2026-06-22 14:16:07 +0200

    add link for booking flight

--- notification_manager.py
+++ notification_manager.py
@@ -1,18 +1,28 @@
 import os
+
+import requests
 from twilio.rest import Client
 
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
     def __init__(self):
-        self.account_sid = os.getenv('ACCOUNT_SID')
-        self.auth_token_twilio = os.getenv('AUTH_TOKEN_TWILIO')
+        self.account_sid = os.getenv("ACCOUNT_SID")
+        self.auth_token_twilio = os.getenv("AUTH_TOKEN_TWILIO")
         self.from_number = os.environ.get("TWILIO_FROM")
         self.to_number = os.environ.get("TWILIO_TO")
 
     def send_sms(self, price, iata_origin, iata_destination, from_date, to_date):
+        long_link = (f"https://www.google.com/travel/flights?"
+                     f"q=flights+from+{iata_origin}"
+                     f"+to+{iata_destination}"
+                     f"+{from_date}+{to_date}")
+
+        response = requests.get(f"http://tinyurl.com/api-create.php?url={long_link}")
+        short_link = response.text
+
         client = Client(self.account_sid, self.auth_token_twilio)
         message = client.messages.create(
-            body=f"Low price alert! Only {price} euro to fly from {iata_origin} to {iata_destination}, on {from_date} until {to_date}",
+            body=f"Low price alert! Only {price} euro to fly from {iata_origin} to {iata_destination}, on {from_date} until {to_date}. Book: {short_link}",
             from_=self.from_number,
             to=self.to_number,
         )
